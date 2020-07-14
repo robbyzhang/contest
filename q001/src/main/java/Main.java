618e3e0c72b94982258263255f32f61d770dd550
Robby Zhang <robby.zhang@nokia-sbell.com>
2020-07-14 14:16:43 +0800

    1

---
+++ q001/src/main/java/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode ret = solution.addTwoNumbers(toListNode("342"), toListNode("465"));
+        System.out.println(eq(ret, "807"));
+    }
+
+    static ListNode toListNode(String s){
+        ListNode head = null;
+        ListNode tail = null;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(head == null){
+                head = new ListNode(ch - '0');
+                tail = head;
+            }else{
+                tail.next = new ListNode(ch - '0');
+                tail = tail.next;
+            }
+        }
+
+        return head;
+    }
+
+    static boolean eq(ListNode listNode, String s){
+        ListNode p = listNode;
+        String ret = "";
+        while(p!=null){
+            ret = p.val + ret;
+            p = p.next;
+        }
+        System.out.println(ret);
+
+        return ret.equals(s);
+    }
+}
