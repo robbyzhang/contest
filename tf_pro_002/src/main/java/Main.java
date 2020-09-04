9996523767101de774623bba1ab645a241fde834
Robby Zhang <robby.zhang@nokia-sbell.com>
2020-09-04 11:03:00 +0800

    1

--- tf_pro_002/src/main/java/Main.java
+++ tf_pro_002/src/main/java/Main.java
@@ -26,16 +26,39 @@ public class Main {
         }
 
 
-        CorrectSolution correctSolution = new CorrectSolution();
-        int expected = correctSolution.numIslands(map);
+        Main main = new Main();
+        int expected = main.numIslands(map);
 
         Solution solution = new Solution();
-        int actual = solution.numIslands(map);
+        int actual = solution.numIslands(map1);
 
         if(expected != actual){
             String input = mapToString(map1);
             String error = String.format("Input : %s\nExpected : %s\nActual : %s\n", input, expected, actual);
             throw new RuntimeException(error);
+        }
+    }
+
+    private static int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int counter = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') return;
+        grid[i][j] = '0'; // mark as visited
+        for (int[] dir : directions) {
+            dfs(grid, i + dir[0], j + dir[1]);
         }
     }
 
