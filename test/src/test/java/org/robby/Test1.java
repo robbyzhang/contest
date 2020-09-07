504cca24c9d2adbcd32de2e40d4dd7e9ce2ad4f7
Robby Zhang <robby.zhang@nokia-sbell.com>
2020-09-07 17:27:52 +0800

    1

--- test/src/test/java/org/robby/Test1.java
+++ test/src/test/java/org/robby/Test1.java
@@ -4,16 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Test1 extends BaseTest{
-    public final int questionId = 180;
+    public final int questionId = 188;
     public final int expectedPoints = 15;
 
     @Test
     public void test_correct()  {
         AnswerResponse resp = (AnswerResponse) httpPost(baseUrl + "/api/questionAnswer/test",
-                new Answer(0, questionId, Util.readFileToBase64("180.correct.txt")),
+                new Answer(0, questionId, Util.readFileToBase64("188.correct.txt")),
                 AnswerResponse.class);
 
-        System.out.println(resp.getResult());
+        System.out.println(resp.getScore());
         Assert.assertEquals(expectedPoints, resp.getScore());
         Assert.assertTrue(resp.isCorrect());
         Assert.assertEquals(resp.getQuestionId(), questionId);
